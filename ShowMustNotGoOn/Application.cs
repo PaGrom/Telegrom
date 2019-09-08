b58fc82ecc48e22bb7f73968f7c0ff1d15b737f7
PaGrom <pagrom23@gmail.com>
2019-09-08 23:43:59 +0300

    Add solution and hello world

---
+++ ShowMustNotGoOn/Application.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Serilog;
+using Telegram.Bot;
+
+namespace ShowMustNotGoOn
+{
+    public class Application
+    {
+        private readonly ITelegramBotClient _telegramBotClient;
+        private readonly ILogger _logger;
+
+        public Application(ITelegramBotClient telegramBotClient,ILogger logger)
+        {
+            _telegramBotClient = telegramBotClient;
+            _logger = logger;
+        }
+
+        public async Task RunAsync()
+        {
+            _logger.Information("Application start");
+            var me = await _telegramBotClient.GetMeAsync();
+            _logger.Information($"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+        }
+    }
+}
---
+++ ShowMustNotGoOn/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Configuration;
+using System.Threading.Tasks;
+using Autofac;
+using Serilog;
+using Telegram.Bot;
+
+namespace ShowMustNotGoOn
+{
+    public static class Program
+    {
+        private static IContainer CompositionRoot()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.Register<ILogger>((c, p) => new LoggerConfiguration()
+                    .MinimumLevel.Information()
+                    .WriteTo.Console()
+                    .WriteTo.RollingFile(
+                        Path.Combine(ConfigurationManager.AppSettings["LogFolder"], "Log-{Date}.txt"))
+                    .CreateLogger())
+                .SingleInstance();
+
+            builder.RegisterInstance(new TelegramBotClient(ConfigurationManager.AppSettings["TelegramApiToken"]))
+                .AsImplementedInterfaces();
+
+            builder.RegisterType<Application>();
+
+            return builder.Build();
+        }
+        public static async Task Main()
+        {
+            using var scope = CompositionRoot().BeginLifetimeScope();
+            await scope.Resolve<Application>().RunAsync();
+        }
+    }
+}
