752e59d7e2236998ecd1033740183ce195cc818a
PaGrom <pagrom23@gmail.com>
2020-06-09 01:35:36 +0300

    Optimize references

--- Telegrom.StateMachine/Builder/GeneratedState.cs
+++ Telegrom.StateMachine/Builder/GeneratedState.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Telegrom.StateMachine.Attributes;
 
 namespace Telegrom.StateMachine.Builder
