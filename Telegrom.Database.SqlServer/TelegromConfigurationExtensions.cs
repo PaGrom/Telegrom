8dce0036a2b0092bbf764ed759e74b5dd5c12a09
PaGrom <pagrom23@gmail.com>
2020-08-24 12:27:48 +0300

    Add SqlServer support

---
+++ Telegrom.Database.SqlServer/TelegromConfigurationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Telegrom.Core.Configuration;
+
+namespace Telegrom.Database.SqlServer
+{
+    public static class TelegromConfigurationExtensions
+    {
+        public static ITelegromConfiguration UseSqlServerDatabase(
+            this ITelegromConfiguration configuration,
+            string connectionString,
+            Action<DbContextOptionsBuilder> action)
+        {
+            _ = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseSqlServer(connectionString);
+
+            action(optionsBuilder);
+
+            return configuration.Use(DatabaseOptions.Current, x => DatabaseOptions.Current = optionsBuilder);
+        }
+    }
+}
